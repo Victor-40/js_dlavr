1d058d385fa80d6553666a18b084ce3ec68f6184
Victor Cherezov <victor_cherezov@mentor.com>
2019-09-24 18:13:01 +0300

    first attempt

--- lesson2/4-form/js/scripts.js
+++ lesson2/4-form/js/scripts.js
@@ -1,5 +1,35 @@
 window.addEventListener('load', function(e){
+    
+    let myForm = document.querySelector('.mySubmit');
+    let inputFields = document.querySelectorAll('.check');
+    let minInputLength = 2;
 
-	
-	
+    myForm.addEventListener('submit', validateForm);
+ 
+    for(let i=0; i < inputFields.length; i++){
+        inputFields[i].addEventListener('input', checkInput);
+    }
+    
+    function checkInput(){
+        if(this.value.length >= minInputLength) {
+            this.className = this.className.replace(/ err/g, "");
+        }
+    }
+
+    function validateForm(e) {
+        let emptyFields = false;
+        inputFields.forEach(function(element){ 
+            if(element.value.length < minInputLength){
+                emptyFields = true;
+                if(!element.className.includes("err")){
+                    element.className += "  err";
+                }
+            }
+        });
+
+        if(emptyFields){
+            e.preventDefault();
+            return false;
+        }
+    }
 });
